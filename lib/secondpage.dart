7e389ba19e46da109f4d9578051868a0904cbbab
shadow <anuragverma3790@gmail.com>
2025-04-19 20:32:59 +0530

    add column and children

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -5,8 +5,8 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body : Center(
-        child : Text("hello world"),
+      body : Column(
+        children : [Text("0"),]
       ),
     );
   }

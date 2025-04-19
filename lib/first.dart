47d33394d177956957712e51a5cdca5877f087c3
shadow <anuragverma3790@gmail.com>
2025-04-19 17:45:32 +0530

    const add

--- lib/first.dart
+++ lib/first.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/widgets.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp( const MyApp());
 }
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
   @override
   Widget build (BuildContext context){
     return Text(

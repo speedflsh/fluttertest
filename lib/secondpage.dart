5bca49091abe8d2c856385d7300721777297f60f
shadow <anuragverma3790@gmail.com>
2025-04-20 15:07:44 +0530

    addcenter

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -3,11 +3,15 @@ import "package:flutter/material.dart";
 class CurrencyConverterMaterialPage extends StatelessWidget{
   const CurrencyConverterMaterialPage({super.key});
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body : Column(
-        children : [Text("0"),]
-      ),
-    );
-  }
+ Widget build(BuildContext context){
+  return const Scaffold(
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[Text('0')]
+      )
+    )
+  );
+
+ }
 }

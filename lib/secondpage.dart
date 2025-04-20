29e09762722e95b39a9790e1e5fff506ca879c67
shadow <anuragverma3790@gmail.com>
2025-04-20 16:05:51 +0530

    addtextstyle

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -5,13 +5,19 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
   @override
  Widget build(BuildContext context){
   return const Scaffold(
+    backgroundColor: Color.fromRGBO(9, 0, 2, 1),
     body: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children:[Text('0')]
-      )
-    )
-  );
+        children:[
+          Text('0',
+          style : TextStyle(
+            fontSize: 55,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ))
+        ],)
+    ));
 
  }
 }
